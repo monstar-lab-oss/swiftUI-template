a0ddcbc51802b943e1b8b05dd2ead8b01cafdd6f
dinakar-m <dinakar.maurya@monstar-lab.com>
2022-04-19 14:10:26 +0900

    Added r.swift in project with image example

---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/Common/RxSwiftHelper.swift
@@ -0,0 +1,30 @@
+//
+//  RxSwiftHelper.swift
+//  {{cookiecutter.app_name}}
+//
+//  Created by dinakar maurya on 2022/04/19.
+//
+
+import SwiftUI
+import Rswift
+
+// MARK: - ImageResource
+extension ImageResource {
+    var image: Image {
+        Image(name)
+    }
+}
+
+// MARK: - ColorResource
+extension ColorResource {
+    var color: Color {
+        Color(name)
+    }
+}
+
+// MARK: - FontResource
+extension FontResource {
+    func font(size: CGFloat) -> Font {
+        Font.custom(fontName, size: size)
+    }
+}
